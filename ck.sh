68096e6691cbe8baa11ab3a92a782a8bec52945b
Ad-AM <87221134+BCKS-AdAM@users.noreply.github.com>
2021-07-23 15:06:04 +0630

    Update ck.sh

--- ck.sh
+++ ck.sh
@@ -31,13 +31,14 @@ echo -e "\e[1;91;106m           Code By Ad A M \e[0m"
 echo""
 echo -e "\e[5;32m                I am Hackcat \e[0m"
 echo ""
-read -p " Enter your  Hash password : " h
+read -p "     Enter your  Hash password : " h
+echo -e "\e[1;31m            Please with \e[0m"
 while $IFS read ghost
 do
 hash=$(echo $ghost | md5sum |sed 's/[^a-z0-9]//g')
 if [[ $h == $hash ]];then
 clear
-echo "✓ We have a search for the password >> $ghost"
+echo -e  "\e[1;32m   ✓ We have a search for the password >> $ghost \e[0m "
 exit
 else
 echo "no" > /dev/null 2>1
@@ -46,4 +47,4 @@ fi
 
 done < ghost.txt
 clear
-echo "We did not find the password for you"
+echo -e  "\e[1;31m     We did not find the password for you \e[0m"
